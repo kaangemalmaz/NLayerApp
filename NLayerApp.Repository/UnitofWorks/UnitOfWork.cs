263d743898825c37a512df9a7b9ae9b1444cb29e
DESKTOP-46BN8NO\Kaan <n0syste3m65@hotmail.com>
2022-03-06 21:49:00 +0300

    unitofwork

---
+++ NLayerApp.Repository/UnitofWorks/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using NLayerApp.Core.UnitOfWorks;
+
+namespace NLayerApp.Repository.UnitofWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public void Commit()
+        {
+            _appDbContext.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            //result = asenkron metodu sekrona çevirir.
+
+            await _appDbContext.SaveChangesAsync();
+        }
+    }
+}
