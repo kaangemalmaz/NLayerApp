e797cf8bd42d6af9598648090e4e127190d48a46
DESKTOP-46BN8NO\Kaan <n0syste3m65@hotmail.com>
2022-03-07 00:28:25 +0300

    CustomResponseDto(Result yapısı)

---
+++ NLayerApp.Core/Dtos/CustomResponseDto.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NLayerApp.Core.Dtos
+{
+    public class CustomResponseDto<T>
+    {
+        public T Data { get; set; }
+
+        [JsonIgnore]
+        public int StatusCode { get; set; }
+
+        public List<String> Errors { get; set; }
+
+
+        //bu yapı factory design patterdan gelir.
+        //bir sınıf içinde kendi kendinin static olarak set edip ona göre dönüş yapıyorsa factory yani bir fabrika tarzı ile kendi işlemini kendi yapar.
+        //new anahtar sözcüğü kullanma kısmını sadece kendi içinde yapar.
+        //design patter amacı nesne üretmeyi azaltmak clienttan tamamen bağımsız hale getirmektir.
+        public static CustomResponseDto<T> Success(int statusCode)
+        {
+            return new CustomResponseDto<T> { StatusCode = statusCode };
+        }
+        public static CustomResponseDto<T> Success(int statusCode, T data)
+        {
+            return new CustomResponseDto<T> { StatusCode = statusCode, Data = data };
+        }
+
+        public static CustomResponseDto<T> Error(int statusCode, List<string> errors)
+        {
+            return new CustomResponseDto<T> { StatusCode = statusCode, Errors = errors };
+        }
+        public static CustomResponseDto<T> Error(int statusCode, string error)
+        {
+            return new CustomResponseDto<T> { StatusCode = statusCode, Errors = new List<string> { error } };
+        }
+
+    }
+}
---
+++ NLayerApp.Core/Dtos/NoContentDto.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NLayerApp.Core.Dtos
+{
+    public class NoContentDto
+    {
+        [JsonIgnore]
+        public int StatusCode { get; set; }
+
+        public List<String> Errors { get; set; }
+
+
+        //bu yapı factory design patterdan gelir.
+        //bir sınıf içinde kendi kendinin static olarak set edip ona göre dönüş yapıyorsa factory yani bir fabrika tarzı ile kendi işlemini kendi yapar.
+        //new anahtar sözcüğü kullanma kısmını sadece kendi içinde yapar.
+        //design patter amacı nesne üretmeyi azaltmak clienttan tamamen bağımsız hale getirmektir.
+        public static NoContentDto Success(int statusCode)
+        {
+            return new NoContentDto { StatusCode = statusCode };
+        }
+
+        public static NoContentDto Error(int statusCode, List<string> errors)
+        {
+            return new NoContentDto { StatusCode = statusCode, Errors = errors };
+        }
+        public static NoContentDto Error(int statusCode, string error)
+        {
+            return new NoContentDto { StatusCode = statusCode, Errors = new List<string> { error } };
+        }
+    }
+}
