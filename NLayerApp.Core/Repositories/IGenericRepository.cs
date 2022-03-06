588a0b6a84d48f404e816c79ccdb87730d2d6a80
DESKTOP-46BN8NO\Kaan <n0syste3m65@hotmail.com>
2022-03-06 21:32:27 +0300

    Generic Repository

--- NLayerApp.Core/Repositories/IGenericRepository.cs
+++ NLayerApp.Core/Repositories/IGenericRepository.cs
@@ -7,20 +7,20 @@ namespace NLayerApp.Core.Repository
         //select
         //===========================================================
         Task<T> GetByIdAsync(int id);
-        IQueryable<T> GetAllAsync(Expression<Func<T, bool>> expression);
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
         //Buradaki T entity, bool ise dönüş tipidir x>5 denirse her bir satır için 5 den büyükse true değilse false döner. yani trueları getirir.
         IQueryable<T> Where(Expression<Func<T,bool>> expression);
-        Task<T> AnyAsync(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
 
         //insert - delete - update
         //===========================================================
         //add async var çünkü memoride uzun süren bir işlemdir.
-        Task AddAsync(T Tentity);
+        Task AddAsync(T entity);
         Task AddRangeAsync(IEnumerable<T> entities);
         //sadece statusünü değiştiren yapılardır. Uzun süren yapılar olmadığı için asenkron metodları yoktur.
         //async in amacı uzun süren yapılar yüzünden blok yememek için kısa süren işlemler için async yoktur.
-        void Update(T Tentity);
-        void Remove(T Tentity);
+        void Update(T entity);
+        void Remove(T entity);
         void RemoveRange(IEnumerable<T> entities);
     }
 }
---
+++ NLayerApp.Repository/Repositories/GenericRepository.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayerApp.Core.Repository;
+using System.Linq.Expressions;
+
+namespace NLayerApp.Repository.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        //özel bir sorgu yazmak istersen diye burayı protected yapıyorum.
+        //readonly olunca ya tanımlandığı noktada yada constructor da değer atılabilir demektir. Başka yerde set edilemez. !!
+        protected readonly AppDbContext _appDbContext;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+            _dbSet = _appDbContext.Set<T>();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);  
+        }
+
+        public async Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _dbSet.AddRangeAsync(entities);
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<T, bool>> expression)
+        {
+            return await _dbSet.AnyAsync(expression);
+        }
+
+        public IQueryable<T> GetAll(Expression<Func<T, bool>> expression)
+        {
+            //burada asnotracking demek efcore çekmiş olduğu datayı memory alıp tek tek durumlarını izlemesin diyedir. Önemli!
+            //10bin kaydı alır tek tek izlerse performansı sıkıntıya sokar.
+            return _dbSet.AsNoTracking().AsQueryable();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public void Remove(T entity)
+        {
+
+            //burada direk olarak silme yok aslında. burada efcore aslında silinecek datanın statini silinecek olarak işaretliyor. savechanges gelene kadar bekliyor.
+            //yüklü bir işlem olmadığı için remove a gerek yok.
+            _dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public IQueryable<T> Where(Expression<Func<T, bool>> expression)
+        {
+            return _dbSet.Where(expression);
+        }
+    }
+}
