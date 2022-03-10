532dff2b566254b7ab0cd9babc5dac3a781dd025
DESKTOP-46BN8NO\Kaan <n0syste3m65@hotmail.com>
2022-03-10 17:36:16 +0300

    Caching-1

---
+++ NLayerApp.Caching/ProductServiceWithCaching.cs
@@ -0,0 +1,86 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Caching.Memory;
+using NLayerApp.Core.Dtos;
+using NLayerApp.Core.Models;
+using NLayerApp.Core.Repositories;
+using NLayerApp.Core.Services;
+using NLayerApp.Core.UnitOfWorks;
+using System.Linq.Expressions;
+
+namespace NLayerApp.Caching
+{
+    public class ProductServiceWithCaching : IProductService
+    {
+        private const string CacheProductKey = "productCache";
+        private readonly IMapper _mapper;
+        private readonly IMemoryCache _memoryCache;
+        private readonly IProductRepository _productRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductServiceWithCaching(IMapper mapper, IMemoryCache memoryCache, IProductRepository productRepository, IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _memoryCache = memoryCache;
+            _productRepository = productRepository;
+            _unitOfWork = unitOfWork;
+
+            //trygetvalue verilen keye karşılık outda tuttuğu datayı döner. out keywordü ile birden fazla değer dönülebilir.
+            //burada CacheProductKey bunda herhangi bir bilgi olup olmadığı bilgisini alacağız çünkü bu trygetvalue bool döner out _ olup olmadığını döner direk olarak bool.
+            if (!_memoryCache.TryGetValue(CacheProductKey, out _))
+            {
+                //eğer yoksa al tüm productları set et demektir bu.
+                _memoryCache.Set(CacheProductKey, _productRepository.GetAll().ToList());
+            }
+        }
+
+        public Task<Product> AddAsync(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> AddRangeAsync(IEnumerable<Product> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AnyAsync(Expression<Func<Product, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CustomResponseDto<List<ProductWithCategoryDto>>> GetProductsWithCategory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveRangeAsync(IEnumerable<Product> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Product> Where(Expression<Func<Product, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
